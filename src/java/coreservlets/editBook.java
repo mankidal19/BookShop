f3cdde6f9735619ee0790d54056551dd592fba18
NURUL AIMAN <NURUL AIMAN@DESKTOP-E3AB21T>
2017-12-13 22:59:13 +0800

    Merge origin/master

---
+++ src/java/coreservlets/editBook.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coreservlets;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import utils.DBUtils;
+import utils.MyUtils;
+ 
+@WebServlet(urlPatterns = { "/editBook" })
+public class editBook extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    public editBook() {
+        super();
+    }
+ 
+    // Show product edit page.
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection conn = MyUtils.getStoredConnection(request);
+ 
+        String id = (String) request.getParameter("code");
+ 
+        CatalogItem book = null;
+ 
+        String errorString = null;
+ 
+        try {
+            book = DBUtils.findBook(conn, id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+ 
+        // If no error.
+        // The product does not exist to edit.
+        // Redirect to productList page.
+        if (errorString != null && book == null) {
+            response.sendRedirect("viewBookList.jsp");
+            return;
+        }
+ 
+        // Store errorString in request attribute, before forward to views.
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("book", book);
+ 
+        RequestDispatcher dispatcher = request.getServletContext()
+                .getRequestDispatcher("editBookView.jsp");
+        dispatcher.forward(request, response);
+ 
+    }
+ 
+    // After the user modifies the product information, and click Submit.
+    // This method will be executed.
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection conn = MyUtils.getStoredConnection(request);
+ 
+        String id = (String) request.getParameter("itemID");
+        String shortDesc = (String) request.getParameter("shortDescription");
+        String longDesc = (String) request.getParameter("longDescription");
+        String type = (String) request.getParameter("type");
+        String priceStr = (String) request.getParameter("price");
+        float price = 0;
+        try {
+            price = Float.parseFloat(priceStr);
+        } catch (Exception e) {
+        }
+        CatalogItem book = new CatalogItem(id, shortDesc, longDesc, type, price);
+ 
+        String errorString = null;
+ 
+        try {
+            DBUtils.updateBook(conn, book);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+        // Store infomation to request attribute, before forward to views.
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("book", book);
+ 
+        // If error, forward to Edit page.
+        if (errorString != null) {
+            RequestDispatcher dispatcher = request.getServletContext()
+                    .getRequestDispatcher("editBookView.jsp");
+            dispatcher.forward(request, response);
+        }
+        // If everything nice.
+        // Redirect to the product listing page.
+        else {
+            response.sendRedirect("viewBookList.jsp");
+        }
+    }
+ 
+}
